183b84efd0f4278a22a36c8733761bf50a2644d7
Shipa_2 <79965159+da1la@users.noreply.github.com>
2023-11-20 23:14:09 +0300

    CameraRaw script update

--- scripts/cameraRawInstaller.sh
+++ scripts/cameraRawInstaller.sh
@@ -8,29 +8,23 @@ function main() {
 
     #resources will be remove after installation
     RESOURCES_PATH="$SCR_PATH/resources"
-
-    check_ps_installed
     
     export_var
     install_cameraRaw
-}
-
-function check_ps_installed() {
-    ([ -d "$SCR_PATH" ] && [ -d "$CACHE_PATH" ] && [ -d "$WINE_PREFIX" ] && show_message2 "photoshop installed") || error2 "photoshop not found you should intsall photoshop first"
 }
 
 function install_cameraRaw() {
     local filename="CameraRaw_12_2_1.exe"
     local filemd5="b6a6b362e0c159be5ba1d0eb1ebd0054"
     local filelink="https://download.adobe.com/pub/adobe/photoshop/cameraraw/win/12.x/CameraRaw_12_2_1.exe"
-    local filepath="$CACHE_PATH/$filename"
+    local filepath="$filename"
 
-    download_component $filepath $filemd5 $filelink $filename
+    wget -O CameraRaw_12_2_1.exe "https://download.adobe.com/pub/adobe/photoshop/cameraraw/win/12.x/CameraRaw_12_2_1.exe"
 
     echo "===============| Adobe Camera Raw v12 |===============" >> "$SCR_PATH/wine-error.log"
     show_message2 "Adobe Camera Raw v12 installation..."
 
-    wine $filepath &>> "$SCR_PATH/wine-error.log" || error2 "sorry something went wrong during Adobe Camera Raw v12 installation"
+    wine CameraRaw_12_2_1.exe
 
     notify-send "Photoshop CC" "Adobe Camera Raw v12 installed successfully" -i "photoshop"
     show_message2 "Adobe Camera Raw v12 installed..."
