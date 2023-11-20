d4a426130095664ac08257fe4271d583fb15cda2
Shipa_2 <79965159+da1la@users.noreply.github.com>
2023-11-20 23:07:41 +0300

    Script updated

--- scripts/PhotoshopSetup.sh
+++ scripts/PhotoshopSetup.sh
@@ -2,13 +2,11 @@
 source "sharedFuncs.sh"
 
 function main() {
-    
+
     mkdir -p $SCR_PATH
     mkdir -p $CACHE_PATH
-    
-    setup_log "================| script executed |================"
 
-    is64
+    setup_log "================| script executed |================"
 
     #make sure wine and winetricks package is already installed
     package_installed wine
@@ -18,37 +16,23 @@ function main() {
 
     RESOURCES_PATH="$SCR_PATH/resources"
     WINE_PREFIX="$SCR_PATH/prefix"
-    
+
     #create new wine prefix for photoshop
     rmdir_if_exist $WINE_PREFIX
-    
+
     #export necessary variable for wine
     export_var
-    
-    #config wine prefix and install mono and gecko automatic
-    echo -e "\033[1;93mplease install mono and gecko packages then click on OK button, do not change Windows version from Windows 7\e[0m"
-    winecfg 2> "$SCR_PATH/wine-error.log"
-    if [ $? -eq 0 ];then
-        show_message "prefix configured..."
-        sleep 5
-    else
-        error "prefix config failed :("
-    fi
 
-    sleep 5
-    if [ -f "$WINE_PREFIX/user.reg" ];then
-        #add dark mod
-        set_dark_mod
-    else
-        error "user.reg Not Found :("
-    fi
-   
-    #create resources directory 
+    #config wine prefix and install mono and gecko automatic
+    echo -e "please install mono and gecko packages, change Windows version to Windows 7, then click apply > ok"
+    winecfg -v win7
+    #create resources directory
     rmdir_if_exist $RESOURCES_PATH
 
     # winetricks atmlib corefonts fontsmooth=rgb gdiplus vcrun2008 vcrun2010 vcrun2012 vcrun2013 vcrun2015 atmlib msxml3 msxml6 gdiplus
-    winetricks atmlib fontsmooth=rgb vcrun2008 vcrun2010 vcrun2012 vcrun2013 atmlib msxml3 msxml6
-    
+    winetricks --self-update
+    winetricks -q atmlib fontsmooth=rgb vcrun2008 vcrun2010 vcrun2012 vcrun2013 atmlib msxml3 msxml6
+
     #install photoshop
     sleep 3
     install_photoshopSE
@@ -57,66 +41,59 @@ function main() {
     replacement
 
     if [ -d $RESOURCES_PATH ];then
-        show_message "deleting resources folder"
+        echo "deleting resources folder"
         rm -rf $RESOURCES_PATH
     else
         error "resources folder Not Found"
     fi
 
     launcher
-    show_message "\033[1;33mwhen you run photoshop for the first time it may take a while\e[0m"
-    show_message "Almost finished..."
-    sleep 30
+    echo "when you run photoshop for the first time it may take a while"
+    echo "Almost finished..."
+    sleep 5
 }
 
 function replacement() {
     local filename="replacement.tgz"
-    local filemd5="6441a8e77c082897a99c2b7b588c9ac4"
-    local filelink="https://victor.poshtiban.io/p/gictor/photoshopCC/replacement.tgz"
-    local filepath="$CACHE_PATH/$filename"
+    local filepath="$filename"
 
-    download_component $filepath $filemd5 $filelink $filename
+    wget -O $filename "https://www.dropbox.com/scl/fi/oq1hn78tf77xa63p3hzpx/replacement.tgz?rlkey=ssm600xp46kl0b9i9fj7i8leq&dl=1"
+    mkdir replacement
+    echo "extract replacement component..."
+    tar -xzf $filename -C replacement
 
-    mkdir "$RESOURCES_PATH/replacement"
-    show_message "extract replacement component..."
-    tar -xzf $filepath -C "$RESOURCES_PATH/replacement"
+    rm -rf "$WINE_PREFIX/drive_c/users/$USER/PhotoshopSE/Resources/IconResources.idx"
+    rm -rf "$WINE_PREFIX/drive_c/users/$USER/PhotoshopSE/Resources/PSIconsHighRes.dat"
+    rm -rf "$WINE_PREFIX/drive_c/users/$USER/PhotoshopSE/Resources/PSIconsLowRes.dat"
 
-    local replacefiles=("IconResources.idx" "PSIconsHighRes.dat" "PSIconsLowRes.dat")
-    local destpath="$WINE_PREFIX/drive_c/users/$USER/PhotoshopSE/Resources"
-    
-    for f in "${replacefiles[@]}";do
-        local sourcepath="$RESOURCES_PATH/replacement/$f"
-        cp -f "$sourcepath" "$destpath" || error "cant copy replacement $f file..."
-    done
+    cd replacement
+    cp -f * "$WINE_PREFIX/drive_c/users/$USER/PhotoshopSE/Resources/" || error "cant copy replacement files..."
+    cd ..
 
-    show_message "replace component compeleted..."
+    echo "replace component compeleted..."
     unset filename filemd5 filelink filepath
 }
 
 function install_photoshopSE() {
     local filename="photoshopCC-V19.1.6-2018x64.tgz"
-    local filemd5="b63f6ed690343ee12b6195424f94c33f"
-    local filelink="https://victor.poshtiban.io/p/gictor/photoshopCC/photoshopCC-V19.1.6-2018x64.tgz"
-    # local filelink="http://127.0.0.1:8080/photoshopCC-V19.1.6-2018x64.tgz"
-    local filepath="$CACHE_PATH/$filename"
+    local filepath="$filename"
 
-    download_component $filepath $filemd5 $filelink $filename
-
-    mkdir "$RESOURCES_PATH/photoshopCC"
-    show_message "extract photoshop..."
-    tar -xzf "$filepath" -C "$RESOURCES_PATH/photoshopCC"
+    #wget -O $filename "https://www.dropbox.com/scl/fi/6xm3hsm3muvb25gpsdycg/photoshopCC-V19.1.6-2018x64.tgz?rlkey=xjspcevujzm7r6cca5yzu72jy&dl=1"
+    mkdir photoshopCC
+    echo "extract photoshop..."
+    tar -xzf "$filename" -C photoshopCC
 
     echo "===============| photoshop CC v19 |===============" >> "$SCR_PATH/wine-error.log"
-    show_message "install photoshop..."
-    show_message "\033[1;33mPlease don't change default Destination Folder\e[0m"
+    echo "install photoshop..."
+    echo "Please don't change default Destination Folder"
 
-    wine64 "$RESOURCES_PATH/photoshopCC/photoshop_cc.exe" &>> "$SCR_PATH/wine-error.log" || error "sorry something went wrong during photoshop installation"
+    wine64 "photoshopCC/photoshop_cc.exe" &>> "$SCR_PATH/wine-error.log" || error "sorry something went wrong during photoshop installation"
     
-    show_message "removing useless helper.exe plugin to avoid errors"
+    echo "removing useless helper.exe plugin to avoid errors"
     rm "$WINE_PREFIX/drive_c/users/$USER/PhotoshopSE/Required/Plug-ins/Spaces/Adobe Spaces Helper.exe"
 
     notify-send "Photoshop CC" "photoshop installed successfully" -i "photoshop"
-    show_message "photoshopCC V19 x64 installed..."
+    echo "photoshopCC V19 x64 installed..."
     unset filename filemd5 filelink filepath
 }
 
